83c5dd58fa3d8fdf856046da029379893be89b81
Tuan <longtuan03@gmail.com>
2021-03-31 00:24:46 +0700

    finished bonus

--- js/looping.js
+++ js/looping.js
@@ -14,7 +14,7 @@ const people = [
     friendsCount: 5000,
     followers: 4000,
     height: "160mm",
-    weight: "70kg"
+    weight: "70kg",
   },
   {
     id: 2,
@@ -31,7 +31,7 @@ const people = [
     friendsCount: 1500,
     followers: 100,
     height: "160mm",
-    weight: "50kg"
+    weight: "50kg",
   },
   {
     id: 3,
@@ -48,7 +48,7 @@ const people = [
     friendsCount: 500,
     followers: 900,
     height: "180mm",
-    weight: "70kg"
+    weight: "70kg",
   },
   {
     id: 4,
@@ -65,7 +65,7 @@ const people = [
     friendsCount: 900,
     followers: 100,
     height: "155mm",
-    weight: "65kg"
+    weight: "65kg",
   },
   {
     id: 5,
@@ -82,7 +82,7 @@ const people = [
     friendsCount: 1500,
     followers: 400,
     height: "140mm",
-    weight: "87kg"
+    weight: "87kg",
   },
   {
     id: 6,
@@ -99,7 +99,7 @@ const people = [
     friendsCount: 1505,
     followers: 10,
     height: "161mm",
-    weight: "74kg"
+    weight: "74kg",
   },
   {
     id: 7,
@@ -116,7 +116,7 @@ const people = [
     friendsCount: 999,
     followers: 540,
     height: "148mm",
-    weight: "54kg"
+    weight: "54kg",
   },
   {
     id: 8,
@@ -133,7 +133,7 @@ const people = [
     friendsCount: 500,
     followers: 300,
     height: "143mm",
-    weight: "89kg"
+    weight: "89kg",
   },
   {
     id: 9,
@@ -150,7 +150,7 @@ const people = [
     friendsCount: 2200,
     followers: 100,
     height: "173mm",
-    weight: "62kg"
+    weight: "62kg",
   },
   {
     id: 10,
@@ -167,7 +167,7 @@ const people = [
       "charcoal gray",
       "Big Apple Red",
       "Bubble Bath",
-      "Kiss me on the Tulips"
+      "Kiss me on the Tulips",
     ],
     favoriteSingers: ["Kanye West"],
     nationality: "USA",
@@ -178,7 +178,7 @@ const people = [
     friendsCount: 2121,
     followers: 212,
     height: "250mm",
-    weight: "90kg"
+    weight: "90kg",
   },
   {
     id: 10,
@@ -195,7 +195,7 @@ const people = [
     friendsCount: 2121,
     followers: 212,
     height: "169mm",
-    weight: "90kg"
+    weight: "90kg",
   },
   {
     id: 11,
@@ -212,7 +212,7 @@ const people = [
     friendsCount: 500,
     followers: 300,
     height: "160mm",
-    weight: "70kg"
+    weight: "70kg",
   },
 ];
 
@@ -223,10 +223,10 @@ const people = [
 // and returns an array of people who are Americans.
 // It should look like this[{ firstName: 'Loi', ...}, { firstName: 'Charles', ...}]
 
-const getAmericans = persons => {
+const getAmericans = (persons) => {
   document.getElementById("getAmericans").innerHTML = persons
     .filter(({ nationality }) => nationality === "USA")
-    .map(person => person.firstName)
+    .map((person) => person.firstName)
     .join(" ");
 };
 
@@ -238,10 +238,10 @@ function onGetAmericans() {
 // and returns an array of people who work at 'CoderSchool'.
 // We would want it to look something like this [{ firstName: 'Loi', ...}, { firstName: 'Charles', ...}]
 
-const getCoderSchoolStaff = persons => {
+const getCoderSchoolStaff = (persons) => {
   document.getElementById("getCoderSchoolStaff").innerHTML = persons
-    .filter(person => person.company === "CoderSchool")
-    .map(staff => `<li>${staff.firstName}</li>`)
+    .filter((person) => person.company === "CoderSchool")
+    .map((staff) => `<li>${staff.firstName}</li>`)
     .join(" ");
 };
 
@@ -255,19 +255,19 @@ function onGetCoderSchoolStaff() {
 // is the most popular.
 // It should look like this { person: { firstName: 'Loi' }, total: 9000 }
 
-const getMostPopularPerson = persons => {
+const getMostPopularPerson = (persons) => {
   let mostPopulerInfo = { person: {}, totalPopularity: 0 };
 
-  persons.map(person => {
+  persons.map((person) => {
     const totalPopularity = person.friendsCount + person.followers;
     if (totalPopularity > mostPopulerInfo.totalPopularity) {
       const newMostPopular = { totalPopularity, person };
       mostPopulerInfo = newMostPopular;
     }
   });
-  document.getElementById("getMostPopularPerson").innerHTML = `${
-    mostPopulerInfo.person.firstName
-  } is the most popular with a score of ${mostPopulerInfo.totalPopularity}`;
+  document.getElementById(
+    "getMostPopularPerson"
+  ).innerHTML = `${mostPopulerInfo.person.firstName} is the most popular with a score of ${mostPopulerInfo.totalPopularity}`;
 };
 
 function onGetMostPopularPerson() {
@@ -282,75 +282,241 @@ function onGetMostPopularPerson() {
 // return an array. The array will contain persons who have nationality as VN for Vietnamese.
 // Make sure the function will account for 'VN' and 'vn'
 // It should look like this [{}, {}, {}, ...]
+function findVietnamese(array) {
+  return array.filter((item) => item.nationality.toLowerCase() == "vn");
+}
+// console.log(findVietnamese(people));
 
 // 5. Define a function countGenders() which will take the people array as an argument and
 // return a string.
 // It should look like this "There are 7 males and 3 males"
 
+function countGenders(array) {
+  let male = 0;
+  let female = 0;
+  array.forEach((item) => {
+    if (item.gender == "m") {
+      male++;
+    } else {
+      female++;
+    }
+  });
+  return `There are ${male} males and ${female} females`;
+}
+// console.log(countGenders(people));
+
 // 6. Define a function findTallest() which will take the people array as an argument and
 // return an string.
 // The string should look like "Charles was the tallest at 250mm"
+
+function findTallest(array) {
+  let sortedArray = array.sort(
+    (a, b) => takeTheInt(a.height) - takeTheInt(b.height)
+  );
+  let h = sortedArray[sortedArray.length - 1].height;
+  let name = sortedArray[sortedArray.length - 1].lastName;
+  return `${name} was the tallest at ${h}`;
+}
+function takeTheInt(str) {
+  let cutUnit = str.slice(0, 3);
+  let result = parseInt(cutUnit);
+  return result;
+}
+// console.log(findTallest(people));
 
 // 7. Define a function findAverageHeight() which will take the people array as an argument and
 // return an string.
 // The string should look like "The average height of our people is 168mm."
 
+function findAverageHeight(array) {
+  let peopleHeight = array.map((item) => takeTheInt(item.height)); //takeTheInt function is at line 320
+  let result = Math.floor(
+    peopleHeight.reduce((sum, current) => sum + current, 0) /
+      peopleHeight.length
+  );
+  return `The average height of our people is ${result}cm`;
+}
+
+// console.log(findAverageHeight(people));
+
 // 8. Define a function findZodiacs() which will take the people array as an argument and
 // return an string. The string should say the users were from these signs
 // The string should look like "We have Cancers, Tauruss, ... among our people"
+
+function findZodiacs(array) {
+  let result = array.map((item) => item.zodiac);
+
+  return `We have ${result.toString()} among our people`;
+}
+// console.log(findZodiacs(people));
 
 // 9. Define a function findAverageAge() which will take the people array as an argument and
 // return an string. The string will have the average age of the people
 // The string should look like "The average age of the people is 21"
 
+function findAverageAge(array) {
+  let ageArray = array.map((item) => item.age);
+  let averageAge = Math.round(
+    ageArray.reduce((total, current) => total + current) / ageArray.length
+  );
+  return `the average age of the people is ${averageAge}`;
+}
+// console.log(findAverageAge(people));
+
 // 10. Define a function findStudents() which will take the people array as an argument and
 // return an array. The array will contain persons who have jobTitle listed as 'Student'.
 // It should look like this [{}, {}, {}]
+function findStudents(array) {
+  let result = array.filter((item) => item.jobTitle.toLowerCase() == "student");
+  return result;
+}
+// console.log(findStudents(people));
 
 // 11. Define a function findNguyens() which will take the people array as an argument and
 // return an array. The array will contain persons whose last names are Nguyen.
 // It should look like this [{}, {}]
 
+function findNguyens(array) {
+  let result = array.filter((item) => item.lastName.toLowerCase() == "nguyen");
+  return result;
+}
+// console.log(findNguyens(people));
+
 // 12. Define a function findAdults() which will take the people array as an argument and
 // return an array. The array will contain all people who's age is above 18 and above
 // It should look like this [{}, {}, {}, ...]
+
+function findAdults(array) {
+  let result = array.filter((item) => item.age >= 18);
+  return result;
+}
+// console.log(findAdults(people));
 
 // 13. Define a function findFavoriteColors() which will take the people array as an argument and
 // return an array. The array will contain UNIQUE colors collected from all persons.
 // It should look like this ['red', 'black', 'pink', ...]
 
+function findFavoriteColors(array) {
+  let arr = array.reduce(
+    (total, current) => total.concat(current.favoriteColors),
+    []
+  );
+  let result = [...new Set(arr)];
+  return result;
+}
+
+// console.log(findFavoriteColors(people));
+
 // 14. Define a function findJobTitles() which will take the people array as an argument and
 // return an array. The array will contain UNIQUE jobTitles.
 // It should look like this ['CEO', 'Instructor', 'Student']
+
+function findJobTitles(array) {
+  let arr = array.reduce(
+    (total, current) => total.concat(current.jobTitle),
+    []
+  );
+  let result = [...new Set(arr)];
+  return result;
+}
+
+// console.log(findJobTitles(people));
 
 // 15. Define a function findBirthYears() which will take the people array as an argument and
 // return an array. The array will contain all years which the people were born in.
 // It should look like this ['1997', '1990', '1987', ...]
 
+function findBirthYears(array) {
+  let arr = array.reduce(
+    (total, current) => total.concat(2021 - current.age),
+    []
+  );
+  let result = [...new Set(arr)]; //delete dubplicates
+  return result;
+}
+
+// console.log(findBirthYears(people));
+
 // 16. Define a function sortOldToYoung() which will take the people array as an argument and
 // return an array. The array will contain all persons sorted from oldest to youngest
 // It should look like this [{}, {}, {}, ...]
+
+function sortOldToYoung(array) {
+  let arr = array.sort((a, b) => b.age - a.age);
+  return arr;
+}
+
+// console.log(sortOldToYoung(people));
 
 // 17. Define a function splitIntoMinorsAndAdults() which will take the people array as an argument and
 // return three arrays. The first array will have two arrays within it.
 // The two nested arrays will contain persons. The 0 index nested array will have minors and the 1 index will have adults.
 // It should look like this [[{}, {}, {}, ...], [{}, {}, {}, ...]]
 
+function splitIntoMinorsAndAdults(array) {
+  let arrOld = [];
+  let arrYoung = [];
+  let arrPeople;
+  array.forEach((item) => {
+    if (item.age > 18) {
+      arrOld.push(item);
+    } else {
+      arrYoung.push(item);
+    }
+  });
+  arrPeople = [arrOld, arrYoung];
+  return arrPeople;
+}
+// console.log(splitIntoMinorsAndAdults(people));
+
 // 18. Define a function addFavoriteHerosToPeople() which will take the people array as an argument and
 // return an array. The array will contain all persons in the original array where each person has
 // a new key(favoriteSuperHeros) with the value of an empty array [].
 // It should look like this [{firstName: 'Loi', favoriteSuperHeros: [], ...}]
 
+function addFavoriteHerosToPeople(array) {
+  array.forEach((item) => (item.favoriteHeros = []));
+  return array;
+}
+// console.log(addFavoriteHerosToPeople(people));
+
 // 19. Define a function addBirthPlacesToPeople() which will take the people array as an argument and
 // return an array. The array will contain all the original persons in the array with an additional
 // key(birthPlace) with the value of a new object {}.
 // It should look like this [{firstName: 'Loi', birthPlace: {}, ...}]
-
+function addBirthPlacesToPeople(array) {
+  array.forEach((item) => (item.birthPlace = {}));
+  return array;
+}
+// console.log(addBirthPlacesToPeople(people));
 // 20. Define a function findMostFavoritedColor() which will take the people array as an argument and
 // return an string. The string will be the color which has the most people that have it in their favoriteColors.
 // In the event there are two colors with equal numbers. Return a string that says 'x and y were really popular!'
 // It should look like this 'black' or 'black and red were really popular!'
 
+function findMostFavoritedColor(array) {
+  let colorWithUsedTime = [];
+  let arrayOfAllColor = array.reduce(
+    (total, current) => total.concat(current.favoriteColors),
+    []
+  );
+  // console.log(arrayOfAllColor);
+  let colors = findFavoriteColors(array); //function bonus 13
+  for (let i = 0; i < colors.length - 1; i++) {
+    let filteredArray = arrayOfAllColor.filter((item) => item == colors[i]);
+    let timeUsed = filteredArray.length;
+    // console.log(filteredArray);
+    colorWithUsedTime.push({ color: colors[i], usedTime: timeUsed });
+  }
+
+  colorWithUsedTime.sort((a, b) => b.usedTime - a.usedTime);
+  if (colorWithUsedTime[0].usedTime > colorWithUsedTime[1].usedTime) {
+    return `${colorWithUsedTime[0].color} is really popular`;
+  } else {
+    return `${colorWithUsedTime[0].color} and ${colorWithUsedTime[0].color} are really popular`;
+  }
+}
+// console.log(findMostFavoritedColor(people));
 // ----------------------------------------------------------------------------------------------------------------------------------------
 // -----> LOOKING AHEAD
 // ----------------------------------------------------------------------------------------------------------------------------------------
